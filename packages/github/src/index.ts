fe5e366ac0e1294e2ce2789f66c78e5483f5e93d
Cristian Tcaci <59696583+Chris0Jeky@users.noreply.github.com>
2026-09-21 18:21:47 +0100

    feat(github): export delivery evidence contract

--- packages/github/src/index.ts
+++ packages/github/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./adapters.js";
 export * from "./client.js";
+export * from "./delivery.js";
 export * from "./demo.js";
 export * from "./types.js";
 export * from "./validation.js";
